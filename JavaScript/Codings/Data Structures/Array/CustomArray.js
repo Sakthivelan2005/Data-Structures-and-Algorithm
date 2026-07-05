74b15be74b02ea1562efd521d99ea5d93920fdc7
Sakthivelan <sakthivelan.shankar@gmail.com>
2026-07-05 19:33:21 +0530

    Initialize Data Structure

---
+++ JavaScript/Codings/Data Structures/Array/CustomArray.js
@@ -0,0 +1,5 @@
+//Array is an ordered collection of elements that can be accessed by using a numerical index.
+const StringArr = ['a','b','c','d','e'];
+const NumberArr = [1,2,3,4,5];
+const BoolenArr = [true, false];
+const MixedArr = ['a' , 1 , true , { object : "objectValue" } , ["Nested Array"] ];
