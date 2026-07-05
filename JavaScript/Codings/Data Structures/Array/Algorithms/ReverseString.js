d51ba08eb1f6efe83d38043be469aec8841f3912
Sakthivelan <sakthivelan.shankar@gmail.com>
2026-07-06 01:19:25 +0530

    Added another code for Reverse String

--- JavaScript/Codings/Data Structures/Array/Algorithms/ReverseString.js
+++ JavaScript/Codings/Data Structures/Array/Algorithms/ReverseString.js
@@ -1,12 +1,18 @@
 //Reverse string Algorithm
 const ReverseString  = (string) => {
-    const array = string.split(''); // split the string into array.
+    const length = string.length; // get the length of the string.
     let reversed = ""; // Initialize a string
-    for (let i = array.length - 1; i >= 0; i--) {
-        reversed += array[i]; //adding array element in reverse order
+    for (let i = length - 1; i >= 0; i--) {
+        reversed += string[i]; //adding array element in reverse order
     }
     return reversed; //return the reversed string
 }
 
 const result = ReverseString("Apple")
 console.log(result);
+
+//Another simple way to reverse string with build-in function
+/*
+const reverse = (string) => string.split("").reverse().join("");
+console.log(reverse("Apple"))
+*/
